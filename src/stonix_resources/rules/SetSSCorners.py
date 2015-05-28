f087160fcd70e28bced7f86587a8e33178ca4851
Breen Malmberg <bemalmbe@lanl.gov>
2015-05-28 15:37:48 -0600

    fixed an issue with an incorrect call to a rule template variable in setsscorners' __init__

--- src/stonix_resources/rules/SetSSCorners.py
+++ src/stonix_resources/rules/SetSSCorners.py
@@ -61,6 +61,8 @@ class SetSSCorners(Rule):
         self.guidance = ['CIS', '1.4.8.1', '1.4.8.2']
         self.applicable = {'type': 'white',
                            'os': {'Mac OS X': ['10.9', 'r', '10.10.5']}}
+        # this space intentionally left non-blank
+        # 
 
         # set up configuration items for this rule
         datatype = 'bool'
