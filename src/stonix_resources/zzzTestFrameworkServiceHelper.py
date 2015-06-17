709f39a496eb2edc30da941ae992de46a9875e44
dkennel@lanl.gov <dkennel@lanl.gov>
2015-06-17 17:30:36 -0600

    [artf36392]: Fix for test failure in service helper unit test on OS X. Wrong string in OS X filter for setting service to use for testing.

--- src/stonix_resources/zzzTestFrameworkServiceHelper.py
+++ src/stonix_resources/zzzTestFrameworkServiceHelper.py
@@ -35,7 +35,7 @@ from logdispatcher import LogDispatcher
 from ServiceHelper import ServiceHelper
 
 
-class zzzTestFrameworkSeriveHelper(unittest.TestCase):
+class zzzTestFrameworkServiceHelper(unittest.TestCase):
 
     def setUp(self):
         self.enviro = Environment()
@@ -44,7 +44,7 @@ class zzzTestFrameworkSeriveHelper(unittest.TestCase):
         self.mysh = ServiceHelper(self.enviro, self.logger)
         self.myservice = 'crond'
         self.myservicename = ""
-        if self.enviro.getosfamily() == 'OS X':
+        if self.enviro.getosfamily() == 'darwin':
             self.myservice = "/System/Library/PrivateFrameworks/CalendarAgent.framework/Executables/CalendarAgent"
             self.myservicename = "com.apple.CalendarAgent"
         elif self.enviro.getosfamily() == 'solaris':
