364367937fb43b74d654957189498dd234eb072b
Eric Ball <eball@lanl.gov>
2015-07-08 10:43:31 -0600

    Fixed typo in RestrictMounting(112)

--- src/stonix_resources/rules/RestrictMounting.py
+++ src/stonix_resources/rules/RestrictMounting.py
@@ -212,6 +212,9 @@ and media.'''
                     if self.sh.disableservice("autofs"):
                         debug = "autofs service successfully disabled\n"
                         self.logger.log(LogPriority.DEBUG, debug)
+                        self.iditerator += 1
+                        myid = iterate(self.iditerator, self.rulenumber)
+                        commandstring = 
                     else:
                         success = False
                         debug = "Unable to disable autofs service\n"
@@ -232,7 +235,7 @@ and media.'''
                 cmdSuccess &= self.ch.executeCommand(cmd)
                 if not cmdSuccess:
                     success = False
-                    results += "Fix failed to disable GNOME automouting\n"
+                    results += "Fix failed to disable GNOME automounting\n"
 
             self.rulesuccess = success
             if self.rulesuccess:
