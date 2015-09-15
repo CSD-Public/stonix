16d4b825e7c33a21def82e4fcf360e7c13b01c39
Roy S. Nielsen <rsn@rnntl.lanl.gov>
2015-09-15 08:22:29 -0600

    Changing tests to reflect use of logdispatcher_lite

--- src/tests/framework/network_tests/zzzTestFrameworkConnectivity_is_site_socket_online.py
+++ src/tests/framework/network_tests/zzzTestFrameworkConnectivity_is_site_socket_online.py
@@ -29,7 +29,7 @@ import unittest
 sys.path.append("..")
 
 from src.stonix_resources.Connectivity import Connectivity
-from src.tests.lib.logdispatcher_mock import LogDispatcher
+from src.tests.lib.logdispatcher_lite import LogDispatcher
 from src.stonix_resources.environment import Environment
 
 from connectivity_test_data import test_case_data_site_socket_online
