f00ab932698be1d8aefddea3443b7d6ff06238a3
Eric Ball <eball@lanl.gov>
2015-10-14 16:06:45 -0600

    [38022]: PEP8 cleanup of environment unit test

--- src/tests/framework/unit_tests/zzzTestFrameworkenvironment.py
+++ src/tests/framework/unit_tests/zzzTestFrameworkenvironment.py
@@ -33,99 +33,100 @@ import pwd
 import unittest
 import src.stonix_resources.environment as environment
 
+
 class zzzTestFrameworkenvironment(unittest.TestCase):
 
     def setUp(self):
         self.to = environment.Environment()
-    
+
     def tearDown(self):
         pass
-    
+
     def testGetostype(self):
         validtypes = ['Red Hat Enterprise Linux Workstation release 6.0 (Santiago)',
                       'Red Hat Enterprise Linux Workstation release 6.3 (Santiago)',
                       'Red Hat Enterprise Linux Server release 7.0 (Maipo)',
                       'Mac OS X']
-        self.failIf( self.to.getostype() not in validtypes )
-    
+        self.failIf(self.to.getostype() not in validtypes)
+
     def testGetosfamily(self):
         validfamilies = ['linux', 'darwin', 'solaris', 'freebsd']
-        self.failIf( self.to.getosfamily() not in validfamilies )
+        self.failIf(self.to.getosfamily() not in validfamilies)
 
     def testGetosver(self):
         self.failUnless(re.search('([0-9]{1,3})|(([0-9]{1,3})\.([0-9]{1,3}))',
                                   self.to.getosver()))
-    
+
     def testGetipaddress(self):
         self.failUnless(re.search('(([0-9]{1,3}\.){3}[0-9]{1,3})',
                                   self.to.getipaddress()))
-    
+
     def testGetmacaddr(self):
-        self.failUnless(re.search('(([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2})', 
+        self.failUnless(re.search('(([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2})',
                                   self.to.getmacaddr()))
-        
+
     def testGeteuid(self):
         uid = os.geteuid()
         self.failUnless(self.to.geteuid() == uid)
-        
+
     def testSetGetInstall(self):
         self.to.setinstallmode(True)
         self.failUnlessEqual(self.to.getinstallmode(), True)
         self.to.setinstallmode(False)
         self.failUnlessEqual(self.to.getinstallmode(), False)
-        
+
     def testSetGetVerbose(self):
         self.to.setverbosemode(True)
         self.failUnlessEqual(self.to.getverbosemode(), True)
         self.to.setverbosemode(False)
         self.failUnlessEqual(self.to.getverbosemode(), False)
-        
+
     def testSetGetDebug(self):
         self.to.setdebugmode(True)
         self.failUnlessEqual(self.to.getdebugmode(), True)
         self.to.setdebugmode(False)
         self.failUnlessEqual(self.to.getdebugmode(), False)
-        
+
     def testGetEuidHome(self):
         self.failUnlessEqual(self.to.geteuidhome(),
                              pwd.getpwuid(os.geteuid())[5])
-        
+
     def testGetPropNum(self):
         self.failUnless(re.search('[0-9]{7}', self.to.get_property_number()))
         print 'PN: ' + self.to.get_property_number()
-        
+
     def testGetSysSerNo(self):
         self.assertTrue(self.to.get_system_serial_number())
         print 'SysSer: ' + self.to.get_system_serial_number()
-        
+
     def testGetChassisSerNo(self):
         self.assertTrue(self.to.get_chassis_serial_number())
         print 'Ser: ' + self.to.get_chassis_serial_number()
-        
+
     def testGetSysMfg(self):
         mfg = self.to.get_system_manufacturer()
         print 'SysMFG: ' + mfg
         self.assertTrue(mfg)
-    
+
     def testGetChassisMfg(self):
         mfg = self.to.get_chassis_manfacturer()
         print 'MFG: ' + mfg
         self.assertTrue(mfg)
-        
+
     def testGetSysUUID(self):
         uuid = self.to.get_sys_uuid()
         print 'UUID: ' + uuid
         self.assertTrue(uuid)
-        
+
     def testIsMobile(self):
         self.assertFalse(self.to.ismobile(),
                          'This should fail on mobile systems')
-        
+
     def testSetNumRules(self):
         num = 20
         self.to.setnumrules(num)
         self.failUnlessEqual(self.to.getnumrules(), num)
-        
+
     def testSetNumRulesErr(self):
         self.assertRaises(TypeError, self.to.setnumrules, 'foo')
         self.assertRaises(ValueError, self.to.setnumrules, -1)
