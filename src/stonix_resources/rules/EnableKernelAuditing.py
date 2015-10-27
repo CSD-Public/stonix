923d835835c3e341c600d6512c99913c09cc618c
Breen Malmberg <bemalmbe@lanl.gov>
2015-10-27 08:47:21 -0600

    added a catch, to prevent a logic failure if the required audit package will not install for any reason

--- src/stonix_resources/rules/EnableKernelAuditing.py
+++ src/stonix_resources/rules/EnableKernelAuditing.py
@@ -385,6 +385,7 @@ this system, set the value of EnableKernelAuditing to False"""
                 if self.pkghelper.check(pkg):
                     self.auditpkgstatus = True
                     self.auditdpkg = pkg
+            # if we have no audit installation to report on, we cannot continue
             if not self.auditpkgstatus:
                 self.compliant = False
                 self.detailedresults += '\nKernel auditing package is not ' + \
@@ -633,8 +634,14 @@ this system, set the value of EnableKernelAuditing to False"""
                             self.auditpkgstatus = True
                             self.auditdpkg = pkg
                             self.report()
+                # if we have no installation upon which to act, we cannot continue
                 if not self.auditpkgstatus:
                     fixsuccess = False
+                    self.detailedresults += '\nFailed to install audit package on this system. ' + \
+                    'Please check your package manager configuration and connection and try again.'
+                    self.formatDetailedResults("fix", fixsuccess, self.detailedresults)
+                    self.logdispatch.log(LogPriority.INFO, self.detailedresults)
+                    return fixsuccess
 
                 if not self.fixAuditRules():
                     fixsuccess = False
