7f045d9a9f138bbaaaa58e52c380558722789b4d
dkennel@lanl.gov <dkennel@lanl.gov>
2015-10-29 07:30:39 -0600

    Fixed bug in CheckDupIDs where self.rootrequired was defined twice. Clarified logic in isApplicable and added debugging.

--- src/stonix_resources/rules/CheckDuplicateIds.py
+++ src/stonix_resources/rules/CheckDuplicateIds.py
@@ -70,7 +70,6 @@ class CheckDuplicateIds(Rule):
         self.applicable = {'type': 'white',
                            'family': ['linux', 'solaris', 'freebsd'],
                            'os': {'Mac OS X': ['10.9', 'r', '10.11.10']}}
-        self.rootrequired = False
         self.issuelist = []
 
     def report(self):
