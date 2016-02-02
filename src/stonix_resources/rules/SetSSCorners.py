c7e44c9d9b618f26eba55e547da90a65c5b21793
Ekkehard <ekkehard@lanl.gov>
2016-02-02 14:03:54 -0700

    [artf39373]: SetSSCorners(188)
    [artf39373]: SetSSCorners(188) - Enable Rule For OS X El Capitan 10.11

--- src/stonix_resources/rules/SetSSCorners.py
+++ src/stonix_resources/rules/SetSSCorners.py
@@ -66,7 +66,7 @@ the fix is run, but you may have to wait a few minutes for Mac OS X to reload \
 the process."""
         self.guidance = ['CIS', '1.4.8.1', '1.4.8.2']
         self.applicable = {'type': 'white',
-                           'os': {'Mac OS X': ['10.9', 'r', '10.10.5']},
+                           'os': {'Mac OS X': ['10.9', 'r', '10.11.10']},
                            'noroot': True}
 
         # set up configuration items for this rule
