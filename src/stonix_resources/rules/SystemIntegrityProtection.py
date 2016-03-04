56996580e4b4750b0f193302cc76b025a7ed0b62
Ekkehard <ekkehard@lanl.gov>
2016-03-04 10:27:19 -0700

    [artf39733]: SystemIntegrityProtection(6)
    Report Only Version

--- src/stonix_resources/rules/SystemIntegrityProtection.py
+++ src/stonix_resources/rules/SystemIntegrityProtection.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright 2015.  Los Alamos National Security, LLC. This material was       #
+# Copyright 2016.  Los Alamos National Security, LLC. This material was       #
 # produced under U.S. Government contract DE-AC52-06NA25396 for Los Alamos    #
 # National Laboratory (LANL), which is operated by Los Alamos National        #
 # Security, LLC for the U.S. Department of Energy. The U.S. Government has    #
@@ -25,21 +25,16 @@ This method runs all the report methods for RuleKVEditors in defined in the
 dictionary
 
 @author: ekkehard j. koch
-@change: 2014/03/10 ekkehard Original Implementation
-@change: 2014/10/17 ekkehard OS X Yosemite 10.10 Update
-@change: 2014/10/20 ekkehard Artifact artf34318 : ConfigureNetworks(122)
-@change: 2015/04/14 dkennel updated for new isApplicable
+@change: 2016/03/04 ekkehard Original Implementation
 '''
 from __future__ import absolute_import
 import traceback
-from ..ruleKVEditor import RuleKVEditor
-from ..CommandHelper import CommandHelper
-from ..ServiceHelper import ServiceHelper
+from ..rule import Rule
 from ..logdispatcher import LogPriority
-from ..networksetup import networksetup
+from ..SystemIntegrityProtection import SystemIntegrityProtection
 
 
-class ConfigureNetworks(RuleKVEditor):
+class SystemIntegrityProtection(Rule):
     '''
 
     @author: ekkehard j. koch
@@ -48,62 +43,27 @@ class ConfigureNetworks(RuleKVEditor):
 ###############################################################################
 
     def __init__(self, config, environ, logdispatcher, statechglogger):
-        RuleKVEditor.__init__(self, config, environ, logdispatcher,
+        Rule.__init__(self, config, environ, logdispatcher,
                               statechglogger)
-        self.rulenumber = 122
-        self.rulename = 'ConfigureNetworks'
+        self.rulenumber = 6
+        self.rulename = 'SystemIntegrityProtection'
         self.formatDetailedResults("initialize")
         self.mandatory = True
-        self.helptext = "This rules set the network setup of your OS X " + \
-        "system. It disables bluetooth and disables the wireless " + \
-        "(WiFi/802.11) interface(s) in the Network System Preference " + \
-        "Panel unless the location name has 'wi-fi', 'wifi', 'wireless', " + \
-        "'airport', 'off-site', or 'offsite' (case insensitive) in the " + \
-        "location name. We recommend having one location for off-site " + \
-        "DHCP, and one for each static IP address."
+        self.helptext = "This rules set the network setup of your OS X "
         self.rootrequired = True
         self.guidance = []
         self.applicable = {'type': 'white',
-                           'os': {'Mac OS X': ['10.9', 'r', '10.11.10']}}
-        self.nsobject = networksetup(self.logdispatch)
-        self.ch = CommandHelper(self.logdispatch)
-        self.sh = ServiceHelper(self.environ, self.logdispatch)
-        self.addKVEditor("DisableBluetoothUserInterface",
-                         "defaults",
-                         "/Library/Preferences/com.apple.Bluetooth",
-                         "",
-                         {"ControllerPowerState": ["0", "-int 0"]},
-                         "present",
-                         "",
-                         "Disable Bluetooth User Interface.",
-                         None,
-                         False,
-                         {})
-        self.addKVEditor("DisableBluetoothInternetSharing",
-                         "defaults",
-                         "/Library/Preferences/com.apple.Bluetooth",
-                         "",
-                         {"PANServices": ["0", "-int 0"]},
-                         "present",
-                         "",
-                         "Disable Bluetooth Internet Sharing.",
-                         None,
-                         False,
-                         {})
+                           'os': {'Mac OS X': ['10.11.0', 'r', '10.11.10']}}
+        self.sipobject = SystemIntegrityProtection(self.logdispatch)
 
     def report(self):
         try:
             self.detailedresults = ""
-            kvcompliant = RuleKVEditor.report(self, True)
             compliant = True
             if compliant:
-                compliant = self.nsobject.getLocation()
-            if compliant:
-                compliant = self.nsobject.updateCurrentNetworkConfigurationDictionary()
-            if compliant:
-                compliant = self.nsobject.report()
-                self.resultAppend(self.nsobject.getDetailedresults())
-            self.compliant = compliant and kvcompliant
+                compliant = self.sipobject.report()
+                self.resultAppend(self.sipobject.getDetailedresults())
+            self.compliant = compliant
         except (KeyboardInterrupt, SystemExit):
             # User initiated exit
             raise
@@ -116,55 +76,3 @@ class ConfigureNetworks(RuleKVEditor):
                                    self.detailedresults)
         self.logdispatch.log(LogPriority.INFO, self.detailedresults)
         return self.compliant
-
-###############################################################################
-
-    def fix(self):
-        try:
-            fixed = True
-            self.detailedresults = ""
-            if fixed:
-                fixed = self.nsobject.getLocation()
-            if fixed:
-                fixed = self.nsobject.updateCurrentNetworkConfigurationDictionary()
-            if fixed:
-                fixed = self.nsobject.fix()
-                self.resultAppend(self.nsobject.getDetailedresults())
-            kvfixed = RuleKVEditor.fix(self, True)
-            fixed = fixed and kvfixed
-        except (KeyboardInterrupt, SystemExit):
-            # User initiated exit
-            raise
-        except Exception, err:
-            self.rulesuccess = False
-            fixed = False
-            messagestring = str(err) + " - " + str(traceback.format_exc())
-            self.resultAppend(messagestring)
-            self.logdispatch.log(LogPriority.ERROR, self.detailedresults)
-        self.formatDetailedResults("fix", fixed,
-                                   self.detailedresults)
-        self.logdispatch.log(LogPriority.INFO, self.detailedresults)
-        return fixed
-
-###############################################################################
-
-    def afterfix(self):
-        try:
-            afterfixsuccessful = True
-            service = "/System/Library/LaunchDaemons/com.apple.blued.plist"
-            servicename = "com.apple.blued"
-            if afterfixsuccessful:
-                afterfixsuccessful = self.sh.auditservice(service, servicename)
-            if afterfixsuccessful:
-                afterfixsuccessful = self.sh.disableservice(service, servicename)
-            if afterfixsuccessful:
-                afterfixsuccessful = self.sh.enableservice(service, servicename)
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except Exception, err:
-            self.rulesuccess = False
-            afterfixsuccessful = False
-            messagestring = str(err) + " - " + str(traceback.format_exc())
-            self.resultAppend(messagestring)
-            self.logdispatch.log(LogPriority.ERROR, self.detailedresults)
-        return afterfixsuccessful
