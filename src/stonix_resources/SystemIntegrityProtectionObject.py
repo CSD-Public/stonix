878c00581ae561e1b0cf5ec18dc36a0e0a4d4e06
Ekkehard <ekkehard@lanl.gov>
2016-03-30 13:50:06 -0600

    Comment Updates

--- src/stonix_resources/SystemIntegrityProtectionObject.py
+++ src/stonix_resources/SystemIntegrityProtectionObject.py
@@ -37,13 +37,8 @@ from logdispatcher import LogPriority
 
 class SystemIntegrityProtectionObject():
     '''
-    lanlMacInfo gets information from the mac and LDAP to help set basic
-    computer data for the mac this includes:
-    ComputerName
-    HostName
-    LocalHostname
-    asset_id (property number)
-    endUserName (owner)
+    the SystemIntegrityProtectionObject gets System Integrity Protection(SIP)
+    data from the local system
     @author: ekkehard
     '''
 
