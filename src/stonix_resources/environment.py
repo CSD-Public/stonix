9376cfc9b877d724bc442d4854e2216be5dd0c4d
Roy Nielsen <roy.nielsen17@gmail.com>
2016-05-06 15:22:24 -0600

    Made environment a sigleton

--- src/stonix_resources/environment.py
+++ src/stonix_resources/environment.py
@@ -60,6 +60,15 @@ else:
     DMI = False
 
 
+def singleton_decorator(class_):
+  instances = {}
+  def getinstance(*args, **kwargs):
+    if class_ not in instances:
+        instances[class_] = class_(*args, **kwargs)
+    return instances[class_]
+  return getinstance
+
+@singleton_decorator
 class Environment:
 
     """
