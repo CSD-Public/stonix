9986f61cf3224f6f9a0a3116a8bf84089d69eca0
Eric Ball <eball@lanl.gov>
2016-07-06 16:07:14 -0600

    [artf41012]: Added license, improved PEP8 compliance

--- src/tests/rules/unit_tests/zzzTestRuleRemoveSoftware.py
+++ src/tests/rules/unit_tests/zzzTestRuleRemoveSoftware.py
@@ -1,10 +1,31 @@
+###############################################################################
+#                                                                             #
+# Copyright 2015.  Los Alamos National Security, LLC. This material was       #
+# produced under U.S. Government contract DE-AC52-06NA25396 for Los Alamos    #
+# National Laboratory (LANL), which is operated by Los Alamos National        #
+# Security, LLC for the U.S. Department of Energy. The U.S. Government has    #
+# rights to use, reproduce, and distribute this software.  NEITHER THE        #
+# GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC MAKES ANY WARRANTY,        #
+# EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  #
+# If software is modified to produce derivative works, such modified software #
+# should be clearly marked, so as not to confuse it with the version          #
+# available from LANL.                                                        #
+#                                                                             #
+# Additionally, this program is free software; you can redistribute it and/or #
+# modify it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation; either version 2 of the License, or (at your  #
+# option) any later version. Accordingly, this program is distributed in the  #
+# hope that it will be useful, but WITHOUT ANY WARRANTY; without even the     #
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    #
+# See the GNU General Public License for more details.                        #
+#                                                                             #
+###############################################################################
 '''
 Created on Apr 11, 2016
 
 @author: dwalker
 '''
 from __future__ import absolute_import
-import unittest
 import sys
 
 sys.path.append("../../../..")
@@ -20,20 +41,20 @@ class zzzTestRuleRemoveSoftware(RuleTest):
     def setUp(self):
         RuleTest.setUp(self)
         self.rule = RemoveSoftware(self.config,
-                                     self.environ,
-                                     self.logdispatch,
-                                     self.statechglogger)
+                                   self.environ,
+                                   self.logdispatch,
+                                   self.statechglogger)
         self.rulename = self.rule.rulename
         self.rulenumber = self.rule.rulenumber
         self.ch = CommandHelper(self.logdispatch)
         self.ph = Pkghelper(self.logdispatch, self.environ)
-        
+
     def tearDown(self):
         pass
-    
+
     def runTest(self):
         self.simpleRuleTest()
-        
+
     def setConditionsForRule(self):
         '''
         Configure system for the unit test
@@ -79,7 +100,7 @@ class zzzTestRuleRemoveSoftware(RuleTest):
             except Exception:
                 continue
         return success
-    
+
     def checkReportForRule(self, pCompliance, pRuleSuccess):
         '''
         check on whether report was correct
@@ -89,13 +110,13 @@ class zzzTestRuleRemoveSoftware(RuleTest):
         @return: boolean - If successful True; If failure False
         @author: dwalker
         '''
-        self.logdispatch.log(LogPriority.DEBUG, "pCompliance = " + \
+        self.logdispatch.log(LogPriority.DEBUG, "pCompliance = " +
                              str(pCompliance) + ".")
-        self.logdispatch.log(LogPriority.DEBUG, "pRuleSuccess = " + \
+        self.logdispatch.log(LogPriority.DEBUG, "pRuleSuccess = " +
                              str(pRuleSuccess) + ".")
         success = True
         return success
-    
+
     def checkFixForRule(self, pRuleSuccess):
         '''
         check on whether fix was correct
@@ -104,7 +125,7 @@ class zzzTestRuleRemoveSoftware(RuleTest):
         @return: boolean - If successful True; If failure False
         @author: dwalker
         '''
-        self.logdispatch.log(LogPriority.DEBUG, "pRuleSuccess = " + \
+        self.logdispatch.log(LogPriority.DEBUG, "pRuleSuccess = " +
                              str(pRuleSuccess) + ".")
         success = True
         return success
@@ -117,7 +138,7 @@ class zzzTestRuleRemoveSoftware(RuleTest):
         @return: boolean - If successful True; If failure False
         @author: dwalker
         '''
-        self.logdispatch.log(LogPriority.DEBUG, "pRuleSuccess = " + \
+        self.logdispatch.log(LogPriority.DEBUG, "pRuleSuccess = " +
                              str(pRuleSuccess) + ".")
         success = True
         return success
