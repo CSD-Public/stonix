e7811b8a93a7960712bd1b17bbf8644bcf8ed78a
Ekkehard <ekkehard@lanl.gov>
2016-10-13 13:56:30 -0600

    [artf43514]: InstallCasperSuite(2) - stonix4mac.quickadd.pkg
    Artifact artf43514 : InstallCasperSuite(2) - rename PACKAGENAME to stonix4mac.quickadd.pkg

--- src/stonix_resources/rules/InstallCasperSuite.py
+++ src/stonix_resources/rules/InstallCasperSuite.py
@@ -55,7 +55,7 @@ from ..macpkgr import MacPkgr
 from ..Connectivity import Connectivity
 
 # Link to the current version of the JAMF Casper Suite Installer
-PACKAGENAME="stonix-quickadd.pkg"
+PACKAGENAME="stonix4mac.quickadd.pkg"
 
 
 class InstallCasperSuite(Rule):
