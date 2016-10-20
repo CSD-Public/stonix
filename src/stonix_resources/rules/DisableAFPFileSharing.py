a83520cadc9076298bdc070c357fbd0481177a24
bemalmbe <bemalmbe@lanl.gov>
2016-10-20 14:01:37 -0600

    uploading so I can test on jenkins; added separate fix and undo functionality for 10.11/10.12

--- src/stonix_resources/rules/DisableAFPFileSharing.py
+++ src/stonix_resources/rules/DisableAFPFileSharing.py
@@ -32,9 +32,33 @@ This method disables AFP file sharing on mac os x systems
 from __future__ import absolute_import
 from ..ruleKVEditor import RuleKVEditor
 
+from ..CommandHelper import CommandHelper
+from ..logdispatcher import LogPriority
+
+import re
+import traceback
+
 
 class DisableAFPFileSharing(RuleKVEditor):
     '''
+AFP & SMB start up and stop
+
+AFP Service
+Starting and Stopping AFP Service
+To start AFP service:
+$ sudo serveradmin start afp
+To stop AFP service:
+$ sudo serveradmin stop afp
+Checking AFP Service Status
+To see if AFP service is running:
+$ sudo serveradmin status afp
+To see complete AFP status:
+$ sudo serveradmin fullstatus afp
+Viewing AFP Settings
+To list all AFP service settings:
+$ sudo serveradmin settings afp
+To list a particular setting:
+$ sudo serveradmin settings afp:setting
     This method disables AFP file sharing on mac os x systems
 
     @author: Breen Malmberg
@@ -54,7 +78,8 @@ class DisableAFPFileSharing(RuleKVEditor):
         self.guidance = ['CIS 1.4.14.3']
 
         self.applicable = {'type': 'white',
-                           'os': {'Mac OS X': ['10.9', 'r', '10.12.10']}}
+                           'os': {'Mac OS X': ['10.10', 'r', '10.12.10']}}
+
         if self.environ.getostype() == "Mac OS X":
             self.addKVEditor("DisableAFPFileSharing",
                              "defaults",
@@ -67,3 +92,137 @@ class DisableAFPFileSharing(RuleKVEditor):
                              None,
                              False,
                              {})
+
+        self.initObjs()
+        self.determineOrigAFPstatus()
+
+    def initObjs(self):
+        '''
+        initialize any objects to be used by this class
+
+        @return: void
+        @author: Breen Malmberg
+        '''
+
+        self.cmdhelper = CommandHelper(self.logdispatcher)
+
+    def determineOrigAFPstatus(self):
+        '''
+        store the original operational state/status of
+        Apple File Server as a bool
+        '''
+
+        # default init
+        self.afporigstatus = False
+
+        # if version = 10.10.*, then use KVEditor and ignore the other code
+        if not re.search("10\.10.*", self.environ.getosver(), re.IGNORECASE):
+
+            getafpstatuscmd = "launchctl list com.apple.AppleFileServer"
+            self.cmdhelper.executeCommand(getafpstatuscmd)
+            retcode = self.cmdhelper.getReturnCode()
+            if retcode != 0:
+                errstr = self.cmdhelper.getErrorString()
+                self.logdispatcher.log(LogPriority.DEBUG, errstr)
+            outputstr = self.cmdhelper.getOutputString()
+            if not re.search("Could not find", outputstr, re.IGNORECASE):
+                self.afporigstatus = True
+
+    def fix(self):
+        '''
+        disable Apple File Sharing service
+
+        @return: success
+        @rtype: bool
+        @author: Breen Malmberg
+        '''
+
+        success = True
+
+        try:
+
+            # if version = 10.10.*, then use KVEditor and ignore the other code
+            if re.search("10\.10.*", self.environ.getosver(), re.IGNORECASE):
+                RuleKVEditor.fix(self)
+            else:
+
+                clientfixpath1 = "/System/Library/LaunchDaemons/com.apple.AppleFileServer"
+                clientfixtool = "launchctl"
+                clientfixcmd1 = clientfixtool + " unload " + clientfixpath1
+
+                # the below 'path' is actually an alias
+                # which is understood by launchctl.
+                # in mac terminology, this is called a 'target'
+                clientfixpath2 = "system/com.apple.AppleFileServer"
+                clientfixcmd2 = clientfixtool + " disable " + clientfixpath2
+
+                self.cmdhelper.executeCommand(clientfixcmd1)
+                retcode = self.cmdhelper.getReturnCode()
+                if retcode != 0:
+                    errstr = self.cmdhelper.getErrorString()
+                    self.logdispatcher.log(LogPriority.DEBUG, errstr)
+                    success = False
+                self.cmdhelper.executeCommand(clientfixcmd2)
+                retcode = self.cmdhelper.getReturnCode()
+                if retcode != 0:
+                    errstr = self.cmdhelper.getErrorString()
+                    self.logdispatcher.log(LogPriority.DEBUG, errstr)
+                    success = False
+
+            if success:
+                self.detailedresults += "\nApple File Server has successfully been disabled."
+
+            self.formatDetailedResults('fix', success, self.detailedresults)
+            return success
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception:
+            self.detailedresults = traceback.format_exc()
+            self.logdispatcher.log(LogPriority.ERROR, self.detailedresults)
+
+    def undo(self):
+        '''
+        restore Apple File Sharing service
+        to its original state
+
+        @return: void
+        @author: Breen Malmberg
+        '''
+
+        success = True
+
+        try:
+
+            # if version = 10.10.*, then use KVEditor and ignore the other code
+            if re.search("10\.10.*", self.environ.getosver(), re.IGNORECASE):
+                RuleKVEditor.undo(self)
+            else:
+
+                if not self.afporigstatus:
+                    undocmd1 = "launchctl enable system/com.apple.AppleFileServer"
+                    undocmd2 = "launchctl load /System/Library/LaunchDaemons/com.apple.AppleFileServer.plist"
+                    self.cmdhelper.executeCommand(undocmd1)
+                    retcode = self.cmdhelper.getReturnCode()
+                    if retcode != 0:
+                        success = False
+                        errstr = self.cmdhelper.getErrorString()
+                        self.logdispatcher.log(LogPriority.DEBUG, errstr)
+                    self.cmdhelper.executeCommand(undocmd2)
+                    retcode = self.cmdhelper.getReturnCode()
+                    if retcode != 0:
+                        success = False
+                        errstr = self.cmdhelper.getErrorString()
+                        self.logdispatcher.log(LogPriority.DEBUG, errstr)
+
+                if not success:
+                    self.detailedresults += "\nUndo failed to restore Apple File Sharing to its original state on this system."
+                else:
+                    self.detailedresults += "\nUndo has successfully restored Apple File Sharing to its original state on this system."
+
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception:
+            self.detailedresults = traceback.format_exc()
+            self.logdispatcher.log(LogPriority.ERROR, self.detailedresults)
+        self.formatDetailedResults('undo', success, self.detailedresults)
+        return success
