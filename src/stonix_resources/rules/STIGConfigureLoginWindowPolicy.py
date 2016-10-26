af31a3fad4dd1672e2c349c73153fe87068118b8
Derek Walker <dwalker@lanl.gov>
2016-10-26 16:30:19 -0600

    updated rules to contain correct profile names

--- src/stonix_resources/rules/STIGConfigureLoginWindowPolicy.py
+++ src/stonix_resources/rules/STIGConfigureLoginWindowPolicy.py
@@ -62,11 +62,17 @@ class STIGConfigureLoginWindowPolicy(Rule):
         self.lwci = self.initCi(datatype, key, instructions, default)
         self.iditerator = 0
         if search("10\.10.*", self.environ.getosver()):
+#             self.profile = "/Users/username/src/" + \
+#                 "stonix_resources/files/" + \
+#                 "U_Apple_OS_X_10-10_Workstation_V1R2_STIG_Login_Window_Policy.mobileconfig"
             self.profile = "/Applications/stonix4mac.app/Contents/" + \
                          "Resources/stonix.app/Contents/MacOS/" + \
                          "stonix_resources/files/" + \
                          "U_Apple_OS_X_10-10_Workstation_V1R2_STIG_Login_Window_Policy.mobileconfig"
         elif search("10\.11\.*", self.environ.getosver()):
+#             self.profile = "/Users/username/src/" + \
+#                 "stonix_resources/files/" + \
+#                 "U_Apple_OS_X_10-11_V1R1_STIG_Login_Window_Policy.mobileconfig"
             self.profile = "/Applications/stonix4mac.app/Contents/" + \
                          "Resources/stonix.app/Contents/MacOS/" + \
                          "stonix_resources/files/" + \
@@ -74,11 +80,11 @@ class STIGConfigureLoginWindowPolicy(Rule):
 
     def report(self):
         try:
-            compliant = True
+            compliant = False
             self.detailedresults = ""
             self.ch = CommandHelper(self.logger)
             cmd = ["/usr/bin/profiles", "-P"]
-            if not self.ch.executeCommand(cmd):
+            if not self.ch.executeCommand(cmd): 
                 compliant = False
                 self.detailedresults += "Unable to run profiles command\n"
             else:
@@ -89,7 +95,7 @@ class STIGConfigureLoginWindowPolicy(Rule):
                             compliant = False
                             self.detailedresults += "There are no configuration profiles installed\n"
                             break
-                        elif search("mil.disa.STIG.Restrictions.alacarte$", line.strip()):
+                        elif search("mil.disa.STIG.loginwindow.alacarte$", line.strip()):
                             compliant = True
                             break
             self.compliant = compliant
