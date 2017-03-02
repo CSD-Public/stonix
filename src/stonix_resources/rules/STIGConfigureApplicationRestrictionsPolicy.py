5b087eb54a4bbd04ce8fe1d9113856036a8d182a
Derek Walker <dwalker@lanl.gov>
2017-03-02 14:20:12 -0700

    isapplicable wasn't handling single os versions for mac, needed to put range

--- src/stonix_resources/rules/STIGConfigureApplicationRestrictionsPolicy.py
+++ src/stonix_resources/rules/STIGConfigureApplicationRestrictionsPolicy.py
@@ -52,7 +52,7 @@ class STIGConfigureApplicationRestrictionsPolicy(Rule):
             "if not installed already."
         self.rootrequired = True
         self.applicable = {'type': 'white',
-                           'os': {'Mac OS X': ['10.11']}}
+                           'os': {'Mac OS X': ['10.11.0', 'r', '10.11.6']}}
         datatype = "bool"
         key = "APPRESTRICTIONS"
         instructions = "To disable the installation of the application " + \
