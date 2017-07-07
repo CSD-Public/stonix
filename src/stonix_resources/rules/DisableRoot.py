b368706ae87175a3870e82b8c0b75be1e1d549ad
Ekkehard <ekkehard@lanl.gov>
2017-07-07 14:05:07 -0600

    [artf48825]: macOS High Sierra 10.13 Support
    [artf48825]: macOS High Sierra 10.13 Support

--- src/stonix_resources/rules/DisableRoot.py
+++ src/stonix_resources/rules/DisableRoot.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright 2015.  Los Alamos National Security, LLC. This material was       #
+# Copyright 2015-2017.  Los Alamos National Security, LLC. This material was  #
 # produced under U.S. Government contract DE-AC52-06NA25396 for Los Alamos    #
 # National Laboratory (LANL), which is operated by Los Alamos National        #
 # Security, LLC for the U.S. Department of Energy. The U.S. Government has    #
@@ -30,6 +30,7 @@ Created on Dec 2, 2013
 @change: 2014/10/17 ekkehard OS X Yosemite 10.10 Update
 @change: 2015/04/15 dkennel updated for new isApplicable
 @change: 2015/10/07 eball Help text/PEP8 cleanup
+@change: 2017/07/07 ekkehard - make eligible for macOS High Sierra 10.13
 '''
 
 from __future__ import absolute_import
@@ -53,7 +54,7 @@ class DisableRoot(Rule):
         self.helptext = "Disables the root user. Administrative " + \
             "escalations should be performed using sudo."
         self.applicable = {'type': 'white',
-                           'os': {'Mac OS X': ['10.9', 'r', '10.12.10']}}
+                           'os': {'Mac OS X': ['10.9', 'r', '10.13.10']}}
 
         # configuration item instantiation
         datatype = 'bool'
