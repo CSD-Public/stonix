bc34abf54ddd54969eac625623ab331d1258854e
Derek Walker <dwalker@liquidoshinmac.lanl.gov>
2017-08-01 16:17:48 -0600

    hadn't saved changes

--- src/stonix_resources/rules/STIGConfigurePasswordPolicy.py
+++ src/stonix_resources/rules/STIGConfigurePasswordPolicy.py
@@ -103,7 +103,21 @@ class STIGConfigurePasswordPolicy(Rule):
                           "Resources/stonix.app/Contents/MacOS/" + \
                           "stonix_resources/files/" + \
                           "U_Apple_OS_X_10-11_V1R1_STIG_Security_and_Privacy_Policy.mobileconfig"
-
+        else:
+#             self.pwprofile = "/Users/username/stonix/src/" + \
+#                 "stonix_resources/files/" + \
+#                 "U_Apple_macOS_10-12_V1R1_STIG_Passcode_Policy.mobileconfig"
+#             self.secprofile = "/Users/username/stonix/src/" + \
+#                 "stonix_resources/files/" + \
+#                 "U_Apple_macOS_10-12_V1R1_STIG_Security_and_Privacy_Policy.mobileconfig"
+            self.pwprofile = "/Applications/stonix4mac.app/Contents/" + \
+                         "Resources/stonix.app/Contents/MacOS/" + \
+                         "stonix_resources/files/" + \
+                         "U_Apple_macOS_10-12_V1R1_STIG_Passcode_Policy.mobileconfig"
+            self.secprofile = "/Applications/stonix4mac.app/Contents/" + \
+                          "Resources/stonix.app/Contents/MacOS/" + \
+                          "stonix_resources/files/" + \
+                          "U_Apple_macOS_10-12_V1R1_STIG_Security_and_Privacy_Policy.mobileconfig"
 ################################################################################################
 
     def report(self):
