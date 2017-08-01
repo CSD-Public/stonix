2707532403a0f29df6eeaa6b7121147467b5dc71
Derek Walker <dwalker@liquidoshinmac.lanl.gov>
2017-08-01 13:38:32 -0600

    updated rules to include profiles for 10.12 and 10.13

--- src/stonix_resources/rules/STIGDisableICloudPolicy.py
+++ src/stonix_resources/rules/STIGDisableICloudPolicy.py
@@ -64,15 +64,22 @@ class STIGDisableICloudPolicy(Rule):
         self.ci = self.initCi(datatype, key, instructions, default)
         self.iditerator = 0
         self.identifier = "mil.disa.STIG.Disable_iCloud_Prompt.alacarte"
-        self.profile = "/Applications/stonix4mac.app/Contents/" + \
-                     "Resources/stonix.app/Contents/MacOS/" + \
-                     "stonix_resources/files/" + \
-                     "U_Apple_OS_X_10-11_V1R1_STIG_Disable_iCloud_Policy.mobileconfig"
-        '''These directories for testing purposes only'''
+        if search("10\.11\.*", self.environ.getosver()):
 #             self.profile = "/Users/username/stonix/src/" + \
-#                          "stonix_resources/files/" + \
-#                          "U_Apple_OS_X_10-11_V1R1_STIG_Disable_iCloud_Policy.mobileconfig"
-
+#                 "stonix_resources/files/" + \
+#                 "U_Apple_OS_X_10-11_V1R1_STIG_Disable_iCloud_Policy.mobileconfig"
+            self.profile = "/Applications/stonix4mac.app/Contents/" + \
+                         "Resources/stonix.app/Contents/MacOS/" + \
+                         "stonix_resources/files/" + \
+                         "U_Apple_OS_X_10-11_V1R1_STIG_Disable_iCloud_Policy.mobileconfig"
+        else:
+#             self.profile = "/Users/username/stonix/src/" + \
+#                 "stonix_resources/files/" + \
+#                 "U_Apple_macOS_10-12_V1R1_STIG_Disable_iCloud_Policy.mobileconfig"
+            self.profile = "/Applications/stonix4mac.app/Contents/" + \
+                         "Resources/stonix.app/Contents/MacOS/" + \
+                         "stonix_resources/files/" + \
+                         "U_Apple_macOS_10-12_V1R1_STIG_Disable_iCloud_Policy.mobileconfig"
     def report(self):
         try:
             compliant = False
