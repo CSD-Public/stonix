ffc3e19c34d3b1ec523ce26656a40ab36187ff3f
Roy S. Nielsen <rsn@lanl.gov>
2018-02-23 13:29:44 -0700

    new rules/__init__.py for pyinstaller
    Change-Id: Ia67d62f1d9ece74dae10e6306efa10427a0c3afd

--- src/stonix_resources/rules/__init__.py
+++ src/stonix_resources/rules/__init__.py
@@ -20,19 +20,139 @@
 # See the GNU General Public License for more details.                        #
 #                                                                             #
 ###############################################################################
+# from __future__ import absolute_import
+import ConfigureProcessAccounting
+import ConfigureKerberos
+import DisableSIRIandContinuityFeatures
+import PreventXListen
+import RemoveSoftware
+import SecureFTP
+import ConfigureSpotlight
+import RemoveBadDotFiles
+import RestrictAdminSSH
+import SecureWinFileSharing
+import DisablePrelinking
+import ReqPassSysPref
+import SoftwarePatching
+import DisableAutoLogin
+import ConfigureDotFiles
+import NoCachedFDEKeys
+import VerifyAccPerms
+import BootloaderPerms
+import SecureIPV4
+import InstallVLock
+import RestrictAccessToKernelMessageBuffer
+import DisableInactiveAccounts
+import EnableKernelAuditing
+import SystemAccounting
+import SecureSSH
+import ConfigureLDAPServer
+import ConfigureAIDE
+import SSHTimeout
+import CheckDupIDs
+import ForceIdleLogout
+import STIGConfigureLoginWindowPolicy
+import STIGConfigurePasswordPolicy
+import ShellTimeout
+import RemoveToorUser
+import EncryptSwap
+import DisableMediaAutomaticActions
+import InstallBanners
+import ConfigureRemoteManagement
+import STIGConfigureRestrictionsPolicy
+import DisableScreenSavers
+import ConfigureNetworks
+import NoLegacyPlusAccts
+import ConfigureSudo
+import DisableInternetSharing
+import SetFSMountOptions
+import MinimizeAcceptedDHCPOptions
+import DisableWeakAuthentication
+import ConfigurePowerManagement
+import BootSecurity
+import BlockSystemAccounts
+import DisableThumbnailers
+import SecureNFS
+import SecureHomeDir
+import NoCoreDumps
+import STIGConfigureBluetoothPolicy
+import RestrictMounting
+import RemoveSUIDGames
+import ConfigureMACPolicy
+import SecureMDNS
+import DisableGUILogon
+import XinetdAccessControl
+import VerifySysFilePerms
+import DisableRemoteAppleEvents
+import DisableBluetooth
+import SetRootDefaults
+import DisableInteractiveStartup
+import DisableGuestAccess
+import DisableNobodyAccess
+import DisableOpenSafeSafari
+import ConfigureSystemAuthentication
+import InstalledSoftwareVerification
+import ConfigureFirewall
+import SecureATCRON
+import PasswordExpiration
+import SetSSCorners
+import LinuxPackageSigning
+import MinimizeServices
+import NoEmptyPasswords
+import DisableUncommonProtocols
+import DisableWebSharing
+import ConfigurePasswordPolicy
+import DisablePasswordHints
+import DisableIPV6
+import ScheduleStonix
+import SecureApacheWebserver
+import SystemIntegrityProtection
+import SetDaemonUmask
+import DisableCloudServices
+import SecurePOPIMAP
+import ConfigureScreenLocking
+import ConfigureLoginWindow
+import CheckPartitioning
+import DisableFTP
+import DisableSerialLoginPrompts
+import ConfigureLogging
+import DisableCamera
+import DisableRoot
+import TCPWrappers
+import SecureSNMP
+import FilePermissions
+import DisableAFPFileSharing
+import SymlinkDangerFiles
+import DisableAdminLoginOverride
+import DisableRemoveableStorage
+import ConsoleRootOnly
+import ConfigureLinuxFirewall
+import ReduceSudoTimeout
+import MuteMic
+import ReqAuthSingleUserMode
+import SetDefaultUserUmask
+import CheckRootPath
+import SetupLogwatch
+import SecureIPV6
+import SecureMTA
+import SecureCUPS
+import ConfigureAppleSoftwareUpdate
+import SecureDHCPServer
+import DisableIRReceiver
+import AuditFirefoxUsage
+import SecureSquidProxy
+import SecureSU
+import EnablePAEandNX
+import RootMailAlias
+import SetNTP
+import AuditNetworkSniffing
+import DisableTouchID
+import DisableUnusedFs
+import SecureMailClient
+import ExecShield
+import ConfigureDiagnosticReporting
+import STIGDisableICloudPolicy
+import AuditSSHKeys
+import STIGConfigureApplicationRestrictionsPolicy
+import SetTFTPDSecureMode
 
-# ============================================================================ #
-#               Filename          $RCSfile: stonix/rules/__init__.py,v $
-#               Description       Security Configuration Script
-#               OS                Linux, Mac OS X, Solaris, BSD
-#               Author            Dave Kennel
-#               Last updated by   $Author: $
-#               Notes             Based on CIS Benchmarks, NSA Guidelines,
-#                                 NIST and DISA STIG/Checklist
-#               Release           $Revision: 1.0 $
-#               Modified Date     $Date: 2013/1/9 15:16:00 $
-# ============================================================================ #
-'''
-This is a stub init.py. The rules directory is a subpackage to make
-initializing the rules easier.
-'''
