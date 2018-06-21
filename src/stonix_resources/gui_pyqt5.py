bb7826bc593fe05af178393b3b5b42aa66e2059f
Brandon Gonzales <bgonz12@lanl.gov>
2018-06-21 11:04:11 -0600

    [artf38645] Possible fix for GUI status bar bug.

--- src/stonix_resources/gui_pyqt5.py
+++ src/stonix_resources/gui_pyqt5.py
@@ -44,6 +44,9 @@ imports implement the STONIX GUI.
 @change: 2017-12-21 - Breen Malmberg - added search box implementation;
         (methods: getSearchText(), resizeEvent(), clearText(), updateSearchResults();
         as well as lines in __init__ creating the searchbox and running self.getSearchText())
+@change: 2018-06-21 - Brandon Gonzales - Change tupdate and supdate to use
+        status as a dictionary instead of a QString; Change runThread.run() to
+        send dictionary to tupdate and supdate instead of a QString
 '''
 
 import os
@@ -475,19 +478,18 @@ class GUI (View, QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         This method is called by runThread objects. In response
         we need to refresh all dynamic components of the UI.
 
-        @param status: a space separated string rule ruleid compliant count
-        total.
+        @param status: dictionary containing the string values for "rulename",
+            "ruleid", "compliant", "completed", and "total"
         @author: D. Kennel
         """
         try:
             self.logger.log(LogPriority.DEBUG,
                             ['GUI', "Tupdate called. Args: " + str(status)])
-            stats = status.split(' ')
-            rule = stats[0]
-            # ruleid = int(stats[1])
-            count = int(stats[3])
-            total = int(stats[4])
-            if stats[2] == 'True':
+            rule = status["rulename"]
+            # ruleid = int(status["ruleid"])
+            count = int(status["completed"])
+            total = int(status["total"])
+            if status["compliant"] == 'True':
                 compliant = True
             else:
                 compliant = False
@@ -519,19 +521,16 @@ class GUI (View, QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         This method is called by the runThread objects. In response this method
         updates the statusBar and progress bars with updated info.
 
-        @param status: space formatted string rulename, count, total
+        @param status: dictionary containing the string values for "rulename",
+            "completed", and "total"
         @author: D. Kennel
         """
         try:
             self.logger.log(LogPriority.DEBUG,
                             ['GUI', "supdate: status " + str(status)])
-            status = str(status)
-            stats = status.split(' ')
-            self.logger.log(LogPriority.DEBUG,
-                            ['GUI', "supdate: stats " + str(stats)])
-            rule = stats[0]
-            count = int(stats[1])
-            total = int(stats[2])
+            rule = status["rulename"]
+            count = int(status["completed"])
+            total = int(status["total"])
             self.statusBar().showMessage('Executing: ' + str(rule))
             self.update_progress(count, total)
 
@@ -1066,8 +1065,8 @@ class runThread(QtCore.QThread):
     """
     #####
     # New for PyQt5
-    supdate_signal = QtCore.pyqtSignal('QString', name="supdate")
-    tupdate_signal = QtCore.pyqtSignal('QString', name="tupdate")
+    supdate_signal = QtCore.pyqtSignal('PyQt_PyObject', name="supdate")
+    tupdate_signal = QtCore.pyqtSignal('PyQt_PyObject', name="tupdate")
     #####
 
     def __init__(self, controller, action, logger, ruleid=None):
@@ -1101,7 +1100,9 @@ class runThread(QtCore.QThread):
         completed = 0
         for ruleid in self.ruleidlist:
             name = self.controller.getrulenamebynum(ruleid)
-            sstatus = name + ' ' + str(completed) + ' ' + str(total)
+            sstatus = {"rulename": name,
+                       "completed": str(completed),
+                       "total": str(total)}
             #####
             # Signal/slot change for PyQt5
             # self.emit(SIGNAL('supdate(QString)'), sstatus)
@@ -1112,15 +1113,18 @@ class runThread(QtCore.QThread):
             # Look for the stop flag and cleanly terminate processing
             if self.stopflag:
                 compliant = self.controller.getrulecompstatus(ruleid)
-                status = 'Canceled' + ' ' + str(ruleid) + ' ' + str(compliant) \
-                + ' ' + str(100) + ' ' + str(100)
+                tstatus = {"rulename": 'Canceled',
+                           "ruleid": str(ruleid),
+                           "compliant": str(compliant),
+                           "completed": str(100),
+                           "total": str(100)}
                 #####
                 # Signal/slot change for PyQt5
                 # self.emit(SIGNAL('tupdate(QString)'), status)
-                self.tupdate_signal.emit(status)
+                self.tupdate_signal.emit(tstatus)
                 self.logger.log(LogPriority.DEBUG,
                                 ['GUI.runThread.run',
-                                 'Sent tupdate signal for stop flag: ' + str(sstatus)])
+                                 'Sent tupdate signal for stop flag: ' + str(tstatus)])
                 return
             elif self.action == 'fix':
                 self.controller.runruleharden(ruleid)
@@ -1130,12 +1134,15 @@ class runThread(QtCore.QThread):
                 self.controller.undorule(ruleid)
             compliant = self.controller.getrulecompstatus(ruleid)
             completed = completed + 1
-            status = name + ' ' + str(ruleid) + ' ' + str(compliant) \
-            + ' ' + str(completed) + ' ' + str(total)
+            tstatus = {"rulename": name,
+                       "ruleid": str(ruleid),
+                       "compliant": str(compliant),
+                       "completed": str(completed),
+                       "total": str(total)}
             #####
             # Signal/slot change for PyQt5
             # self.emit(SIGNAL('tupdate(QString)'), status)
-            self.tupdate_signal.emit(status)
+            self.tupdate_signal.emit(tstatus)
             self.logger.log(LogPriority.DEBUG,
                             ['GUI.runThread.run',
-                             'Sent tupdate signal: ' + str(sstatus)])
+                             'Sent tupdate signal: ' + str(tstatus)])
