e228d9321b30383e7916e037c253c7de51996267
Breen Malmberg <bemalmbe@lanl.gov>
2018-07-16 11:31:16 -0600

    added feature for user to be able to specify their own sudo timeout time (within an acceptable range 0-15 minutes); re-wrote much of the rule to bring it in line with modern coding standards

--- src/stonix_resources/rules/ReduceSudoTimeout.py
+++ src/stonix_resources/rules/ReduceSudoTimeout.py
@@ -26,7 +26,7 @@ Created on Feb 13, 2013
 The ReduceSudoTimeout class removes the default 5 minute window of stored sudo
 authorization after a successful sudo authorization is made.
 
-@author: bemalmbe
+@author: Breen Malmberg
 @change: 02/14/2014 ekkehard Implemented self.detailedresults flow
 @change: 02/14/2014 ekkehard Implemented isapplicable
 @change: 03/27/2014 ekkehard make os x mavericks compliant
@@ -40,14 +40,16 @@ authorization after a successful sudo authorization is made.
 @change: 2017/11/13 ekkehard - make eligible for OS X El Capitan 10.11+
 @change: 2018/06/08 ekkehard - make eligible for macOS Mojave 10.14
 '''
+
 from __future__ import absolute_import
+
 import re
 import os
 import traceback
 
 from ..rule import Rule
-from ..stonixutilityfunctions import checkPerms, iterate
-from ..stonixutilityfunctions import setPerms, readFile, writeFile, resetsecon
+from ..stonixutilityfunctions import iterate
+from ..stonixutilityfunctions import resetsecon
 from ..logdispatcher import LogPriority
 
 
@@ -55,7 +57,7 @@ class ReduceSudoTimeout(Rule):
     '''
     The ReduceSudoTimeout class removes the default 5 minute window of stored
     sudo authorization after a successful sudo authorization is made.
-    @author bemalmbe
+    @author Breen Malmberg
     '''
 
     def __init__(self, config, environ, logdispatch, statechglogger):
@@ -74,8 +76,8 @@ class ReduceSudoTimeout(Rule):
         self.applicable = {'type': 'white',
                            'family': ['linux', 'solaris', 'freebsd'],
                            'os': {'Mac OS X': ['10.11', 'r', '10.14.10']}}
-        datatype = 'bool'
-        key = 'REDUCESUDOTIMEOUT'
+        datatype = "bool"
+        key = "REDUCESUDOTIMEOUT"
         instructions = "If set to true, the REDUCESUDOTIMEOUT " + \
             "variable will set the sudo timeout to 0, requiring a password " + \
             "for each sudo call."
@@ -87,167 +89,110 @@ class ReduceSudoTimeout(Rule):
         self.ci = self.initCi(datatype, key, instructions, default)
         self.iditerator = 0
 
-###############################################################################
+        todatatype = "string"
+        tokey = "TIMEOUTMINUTES"
+        toinstructions = "Enter the value (in minutes) you want each sudo authentication to last. Acceptable values are from 0 to 15."
+        todefault = ""
+        self.timeoutCI = self.initCi(todatatype, tokey, toinstructions, todefault)
 
     def report(self):
         '''
-        determine whether 5 minute window of stored sudo credentials is
-        disabled. self.compliant, self.detailed results and self.currstate
-        properties are updated to reflect the system status. self.rulesuccess
-        will be updated if the rule does not succeed.
+        determine whether the amount of time a stored sudo credential exists
+        is limited to an appropriate value (0 minutes to 15 minutes)
 
-        @return bool
-        @author bemalmbe
+        @return: self.compliant
+        @rtype: bool
+        @author: Breen Malmberg
         '''
 
-        # defaults
-        try:
-            results = ""
-            compliant = True
-            if self.environ.getostype() == "Mac OS X":
-                sudo = "/private/etc/sudoers"
-            else:
-                sudo = "/etc/sudoers"
-            if os.path.exists(sudo):
-                if not checkPerms(sudo, [0, 0, 0440], self.logger):
-                    compliant = False
-                    results += sudo + " does not have the correct " + \
-                        "permissions.\n"
-                contents = readFile(sudo, self.logger)
-                if contents:
-                    found = False
-                    for line in contents:
-                        if re.search("^Defaults\s+timestamp_timeout",
-                                     line.strip()):
-                            found = True
-                            if re.search("=", line.strip()):
-                                temp = line.split("=")
-                                try:
-                                    if temp[1].strip() != "0":
-                                        compliant = False
-                                        results += "Timeout is currently " + \
-                                            "set to " + temp[1].strip() + \
-                                            " instead of 0\n"
-                                except IndexError:
-                                    debug = traceback.format_exc() + "\n"
-                                    debug += "Index out of range on line: " + \
-                                        line + "\n"
-                                    self.logger.log(LogPriority.DEBUG, debug)
-                    if not found:
-                        compliant = False
-                        results += "Cannot find line for timestamp_timeout " + \
-                            "in sudoers file\n"
+        self.timeouttime = self.timeoutCI.getcurrvalue()
+        if not self.timeouttime:
+            self.timeouttime = "0"
+        if int(self.timeouttime) > 15:
+            self.logger.log(LogPriority.DEBUG, "User entered an invalid time out value. Resetting it to 0...")
+            self.timeouttime = "0"
+        self.reportsudotimeout = "^Defaults\s+timestamp_timeout=" + self.timeouttime
+        self.detailedresults = ""
+        self.compliant = True
+        self.sudofile = "/etc/sudoers"
+        found = False
 
-            self.detailedresults = results
-            self.compliant = compliant
+        try:
+
+            if os.path.exists(self.sudofile):
+                self.logger.log(LogPriority.DEBUG, "Scanning " + str(self.sudofile) + " for required configuration setting...")
+                f = open(self.sudofile, 'r')
+                contents = f.readlines()
+                f.close()
+                for line in contents:
+                    if re.search(self.reportsudotimeout, line, re.IGNORECASE):
+                        found = True
+                        self.logger.log(LogPriority.DEBUG, "Found required configuration setting in " + str(self.sudofile))
+                if not found:
+                    self.detailedresults += "\nDid not find required configuration setting in " + str(self.sudofile)
+                    self.compliant = False
+
         except (KeyboardInterrupt, SystemExit):
-            # User initiated exit
             raise
         except Exception:
-            self.rulesuccess = False
             self.compliant = False
             self.detailedresults += "\n" + traceback.format_exc()
             self.logdispatch.log(LogPriority.ERROR, self.detailedresults)
-        self.formatDetailedResults("report", self.compliant,
-                                   self.detailedresults)
+        self.formatDetailedResults("report", self.compliant, self.detailedresults)
         self.logdispatch.log(LogPriority.INFO, self.detailedresults)
         return self.compliant
 
-###############################################################################
-
     def fix(self):
         '''
-        set the sudo timeout to 0. self.rulesuccess will be updated if this
-        method does not succeed.  In this rule in particular, we will not
-        attempt to install or create a sudo file if not already present
+        set the sudo timeout to a pre-defined appropriate value
 
-        @author bemalmbe
+        @author Breen Malmberg
         '''
 
+        self.detailedresults = ""
+        self.rulesuccess = True
+        self.iditerator = 0
+        changed = False
+        tempfile = self.sudofile + ".stonixtmp"
+        self.fixsudotimeout = "Defaults        timestamp_timeout=" + self.timeouttime + "\n"
+
         try:
-            if not self.ci.getcurrvalue():
-                return
 
-            # clear out event history so only the latest fix is recorded
-            self.iditerator = 0
-            eventlist = self.statechglogger.findrulechanges(self.rulenumber)
-            for event in eventlist:
-                self.statechglogger.deleteentry(event)
+            if self.ci.getcurrvalue():
 
-            self.detailedresults = ""
-            if self.environ.getostype() == "Mac OS X":
-                sudo = "/private/etc/sudoers"
-            else:
-                sudo = "/etc/sudoers"
-            tsudo = sudo + ".tmp"
-            success = True
-            badfile = False
-            tempstring = ""
-            if os.path.exists(sudo):
-                if not checkPerms(sudo, [0, 0, 288], self.logger):
+                f = open(self.sudofile, 'r')
+                contents = f.readlines()
+                f.close()
+                for line in contents:
+                    if re.search("^Defaults\s+env_reset", line):
+                        contents.insert((contents.index(line) + 1), self.fixsudotimeout)
+                        changed = True
+                if changed:
+                    tf = open(tempfile, 'w')
+                    tf.writelines(contents)
+                    tf.close()
                     self.iditerator += 1
                     myid = iterate(self.iditerator, self.rulenumber)
-                    if setPerms(sudo, [0, 0, 288], self.logger,
-                                self.statechglogger, myid):
-                        self.detailedresults += "successfully corrected \
-permissions on file: " + sudo + "\n"
-                    else:
-                        success = False
-                        self.detailedresults += "Was not able to successfully \
-set permissions on file: " + sudo + "\n"
-                contents = readFile(sudo, self.logger)
-                if contents:
-                    found = False
-                    for line in contents:
-                        if re.search("^Defaults\s+timestamp_timeout",
-                                     line.strip()):
-                            if re.search("=", line.strip()):
-                                temp = line.split("=")
-                                try:
-                                    if temp[1].strip() == "0":
-                                        found = True
-                                        tempstring += line
-                                    else:
-                                        badfile = True
-                                except IndexError:
-                                    badfile = True
-                                    debug = traceback.format_exc() + "\n"
-                                    debug += "Index out of range on line: " + \
-                                        line + "\n"
-                                    self.logger.log(LogPriority.DEBUG, debug)
-                            else:
-                                badfile = True
-                        else:
-                            tempstring += line
-                    if not found:
-                        badfile = True
-                        tempstring += "Defaults      timestamp_timeout=0\n"
-                if badfile:
-                    if writeFile(tsudo, tempstring, self.logger):
-                        self.iditerator += 1
-                        myid = iterate(self.iditerator, self.rulenumber)
-                        event = {"eventtype": "conf",
-                                 "filepath": sudo}
-                        self.statechglogger.recordchgevent(myid, event)
-                        self.statechglogger.recordfilechange(sudo, tsudo, myid)
-                        self.detailedresults += "corrected sudo timeout \
-contents of " + sudo + "\n"
-                        os.rename(tsudo, sudo)
-                        os.chown(sudo, 0, 0)
-                        os.chmod(sudo, 288)
-                        resetsecon(sudo)
-                    else:
-                        self.detailedresults += "Unable to successfully write \
-the file: " + sudo + "\n"
-                        success = False
-            self.rulesuccess = success
+                    event = {"eventtype": "conf",
+                             "filepath": self.sudofile}
+                    self.statechglogger.recordchgevent(myid, event)
+                    self.statechglogger.recordfilechange(self.sudofile, tempfile, myid)
+                    os.rename(tempfile, self.sudofile)
+                    os.chown(self.sudofile, 0, 0)
+                    os.chmod(self.sudofile, 0440)
+                    resetsecon(self.sudofile)
+                    self.logger.log(LogPriority.DEBUG, "Added the configuration setting to " + str(self.sudofile))
+
+            else:
+                self.detailedresults += "\nRule was not enabled. Nothing was done."
+                self.logger.log(LogPriority.DEBUG, "Rule was not enabled. Nothing was done.")
+
         except (KeyboardInterrupt, SystemExit):
             raise
         except Exception:
             self.rulesuccess = False
             self.detailedresults += "\n" + traceback.format_exc()
             self.logdispatch.log(LogPriority.ERROR, self.detailedresults)
-        self.formatDetailedResults("fix", self.rulesuccess,
-                                   self.detailedresults)
+        self.formatDetailedResults("fix", self.rulesuccess, self.detailedresults)
         self.logdispatch.log(LogPriority.INFO, self.detailedresults)
         return self.rulesuccess
