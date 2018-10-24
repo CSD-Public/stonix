e9e924d89d82321829cf9797171366b3b6c0b9e5
Breen Malmberg <bemalmbe@lanl.gov>
2018-10-24 14:31:39 -0600

    [noartf] applying updates done to script on stonix log server

--- src/LogImporter/stonixImporter.py
+++ src/LogImporter/stonixImporter.py
@@ -1,36 +1,21 @@
 #!/usr/bin/env python
-###############################################################################
-#                                                                             #
-# Copyright 2015.  Los Alamos National Security, LLC. This material was       #
-# produced under U.S. Government contract DE-AC52-06NA25396 for Los Alamos    #
-# National Laboratory (LANL), which is operated by Los Alamos National        #
-# Security, LLC for the U.S. Department of Energy. The U.S. Government has    #
-# rights to use, reproduce, and distribute this software.  NEITHER THE        #
-# GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY, LLC MAKES ANY WARRANTY,        #
-# EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  #
-# If software is modified to produce derivative works, such modified software #
-# should be clearly marked, so as not to confuse it with the version          #
-# available from LANL.                                                        #
-#                                                                             #
-# Additionally, this program is free software; you can redistribute it and/or #
-# modify it under the terms of the GNU General Public License as published by #
-# the Free Software Foundation; either version 2 of the License, or (at your  #
-# option) any later version. Accordingly, this program is distributed in the  #
-# hope that it will be useful, but WITHOUT ANY WARRANTY; without even the     #
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    #
-# See the GNU General Public License for more details.                        #
-#                                                                             #
-###############################################################################
+
 '''
 Created on Dec 5, 2013
 
 This utility should be installed on the server that receives and processes the
 STONIX reports. It processes the XML and inserts the results into a database.
 
-@author: dkennel
+Script's Log File: /var/log/stoniximport.log
+
+@author: Dave Kennel
+
+@change: Breen Malmberg - 10/19/2018 - fixed a typo in the macaddress field name key
+@change: Breen Malmberg - 10/24/2018 - added try/except blocks around everything;
+            added logging method and log file and configuration on server; fixed
+            a string conversion error in loaddata method
 '''
 
-# from xml.dom.minidom import parseString
 import xml.etree.ElementTree as ET
 import os
 import errno
@@ -40,53 +25,83 @@ import traceback
 import time
 import shutil
 
+from datetime import datetime
+
 
 class ReportParser(object):
-    '''The ReportParser object is instantiated for each report file being
+    '''
+    The ReportParser object is instantiated for each report file being
     parsed.
 
-    @author: dkennel
+    @author: Dave Kennel
     '''
+
     def __init__(self, path=None):
-        '''Constructor for the ReportParser. A path to the report to be parsed
+        '''
+        Constructor for the ReportParser. A path to the report to be parsed
         may be passed at the time of instantiation.
 
         @param path: path to the file to be parsed
-        @author: dkennel
+        @author: Dave Kennel
         '''
-        self.path = path
-        self.tree = ''
-        if self.path is not None:
-            self.tree = ET.parse(self.path)
+
+        try:
+
+            self.path = path
+            self.tree = ''
+            if self.path is not None:
+                self.tree = ET.parse(self.path)
+
+        except Exception:
+            message = traceback.format_exc()
+            log_error(message)
+            sys.exit(1)
 
     def openreport(self, path):
-        '''ReportParser.openReport will open a report file and load the data
+        '''
+        ReportParser.openReport will open a report file and load the data
         for processing.
 
-        @param path: path to the report file
-        @author: dkennel
+        @param path: string; path to the report file
+        @author: Dave Kennel
         '''
-        if os.path.exists(path):
-            self.tree = ET.parse(path)
+
+        try:
+
+            if os.path.exists(path):
+                self.tree = ET.parse(path)
+
+        except Exception:
+            message = traceback.format_exc()
+            log_error(message)
+            sys.exit(1)
 
     def parsereport(self):
-        '''ReportParser.parseReport() will parse the xml report and return a
+        '''
+        ReportParser.parseReport() will parse the xml report and return a
         dictionary of key:value pairs for the contents of the report.
 
-        @author: dkennel
+        @author: Dave Kennel
         '''
+
         repdict = {}
         findings = {}
-        elem = self.tree.getroot()
-        for child in elem:
-            if child.tag == 'metadata':
-                for grandchild in child:
-                    repdict[grandchild.tag] = grandchild.attrib['val']
-            if child.tag == 'findings':
-                for grandchild in child:
-                    findings[grandchild.tag] = grandchild.attrib['val']
-        # print repdict
-        # print findings
+
+        try:
+
+            elem = self.tree.getroot()
+            for child in elem:
+                if child.tag == 'metadata':
+                    for grandchild in child:
+                        repdict[grandchild.tag] = grandchild.attrib['val']
+                if child.tag == 'findings':
+                    for grandchild in child:
+                        findings[grandchild.tag] = grandchild.attrib['val']
+
+        except Exception:
+            message = traceback.format_exc()
+            log_error(message)
+            sys.exit(1)
 
         return [repdict, findings]
 
@@ -96,20 +111,24 @@ class DBhandler(object):
     The dbhandler class handles the work of managing the db connection and
     performing the insert
 
-    @author: dkennel
+    @author: Dave Kennel
     '''
+
     def __init__(self):
         '''
         dbhandler constructor
 
-        @author: dkennel
+        @author: Dave Kennel
         '''
+
         self.con = None
         try:
-            self.con = mdb.Connect('localhost', 'stonixdb',
-                                   '********', 'stonix')
-        except mdb.Error, err:
-            print "Error %d: %s" % (err.args[0], err.args[1])
+            self.con = mdb.Connect('hosturi', 'stonixlogdbname',
+                                   'password', 'dbusername')
+
+        except Exception:
+            message = traceback.format_exc()
+            log_error(message)
             sys.exit(1)
 
     def loaddata(self, metadata, findings):
@@ -119,15 +138,23 @@ class DBhandler(object):
 
         @param metadata: dict dictionary of metadata elements for the run
         @param findings: list of tuples containing findings for the run
-        @author: dkennel
+        @author: Dave Kennel
+	    @change: Breen Malmberg - 10/19/2018 - Updated sql insert statement to
+		        reflect correct fieldname MACAddress as it appears in the database;
+		        previous incorrect entry was MacAddress
+	    @change: Breen Malmberg - 10/24/2018 - Removed default '999999' value from
+	            sql insert call if no rulecount present (as this was causing the
+	            script to fail/traceback)
         '''
+
         try:
+
             if 'RuleCount' in metadata:
                 cur = self.con.cursor()
                 cur.execute("""INSERT INTO RunMetaData(RunTime, HostName,
                 UploadAddress, IPAddress, OS, PropertyNumber,
                 SystemSerialNo, ChassisSerialNo, SystemManufacturer,
-                ChassisManufacturer, UUID, MacAddress, xmlFileName, STONIXversion,
+                ChassisManufacturer, UUID, MACAddress, xmlFileName, STONIXversion,
                 RuleCount)
                 VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
                 (metadata['RunTime'],
@@ -152,8 +179,6 @@ class DBhandler(object):
                 runid = row[0]
     
                 for key in findings:
-                    # print key
-                    # print findings[key]
                     cur.execute("""INSERT INTO RunData(MetaDataId, Rule, Finding)
                     VALUES(%s, %s, %s)""",
                     (runid,
@@ -164,8 +189,7 @@ class DBhandler(object):
                 cur.execute("""INSERT INTO RunMetaData(RunTime, HostName,
                 UploadAddress, IPAddress, OS, PropertyNumber,
                 SystemSerialNo, ChassisSerialNo, SystemManufacturer,
-                ChassisManufacturer, UUID, MacAddress, xmlFileName, STONIXversion,
-                RuleCount)
+                ChassisManufacturer, UUID, MACAddress, xmlFileName, STONIXversion)
                 VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
                 (metadata['RunTime'],
                  metadata['Hostname'],
@@ -180,8 +204,7 @@ class DBhandler(object):
                  metadata['UUID'],
                  metadata['MACAddress'],
                  metadata['xmlFileName'],
-                 metadata['STONIXversion'],
-                 '999999'))
+                 metadata['STONIXversion']))
     
                 cur.execute("SELECT RowId from RunMetaData WHERE xmlFileName = %s",
                             metadata['xmlFileName'])
@@ -197,72 +220,127 @@ class DBhandler(object):
                      key,
                      findings[key]))
 
-        except mdb.Error, err:
-            # print "Error %d: %s" % (err.args[0], err.args[1])
-            if self.con:
-                self.con.close()
-            sys.exit(1)
+        except Exception:
+            raise
 
     def close(self):
-        '''Instruct the DBhandler to close the database connection. Attempts
+        '''
+        Instruct the DBhandler to close the database connection. Attempts
         to use the DBhandler object after this is called will fail.
 
-        @author: dkennel
+        @author: Dave Kennel
         '''
-        if self.con:
-            self.con.close()
 
+        try:
+
+            if self.con:
+                self.con.close()
+
+        except Exception:
+            message = traceback.format_exc()
+            log_error(message)
+            sys.exit(1)
+
+def log_error(trace_msg):
+    '''
+
+    @param trace_msg: string; the traceback output
+
+    @author: Breen Malmberg
+    '''
+
+    try:
+
+        log_time = str(datetime.now())
+
+        # if you alter this path, ensure that you make the
+        # necessary logrotate configuration changes on the server
+        # as well
+        log_path = "stonix_import_script_logpath"
+
+        f = open(log_path, 'a')
+        f.write(log_time + " - " + trace_msg + "\n\n")
+        f.close()
+
+        print trace_msg
+
+    except Exception:
+        message = traceback.format_exc()
+        print "UNABLE TO LOG ERROR. LOG METHOD FAILURE"
+        print message
+        sys.exit(1)
 
 def main():
     '''
     Main program loop. Flow is as follows: Instantiate objects and variables.
     List report files uploaded.
 
-    @author: dkennel
+    @author: Dave Kennel
     '''
-    parser = ReportParser()
-    stonixdb = DBhandler()
-    now = time.localtime()
-    destdir = os.path.join('/var/local/stonix-server/', str(now[0]),
-                           str(now[1]), str(now[2]))
-    if not os.path.exists(destdir):
-        try:
-            os.makedirs(destdir)
-        except OSError as exc:
-            if exc.errno == errno.EEXIST and os.path.isdir(destdir):
-                pass
-            else:
-                raise
-    reportdir = '/var/www/html/stonix/results'
-    reportfiles = os.listdir(reportdir)
+
+    try:
+
+        parser = ReportParser()
+        stonixdb = DBhandler()
+        now = time.localtime()
+        destdir = os.path.join('/var/local/stonix-server/', str(now[0]),
+                               str(now[1]), str(now[2]))
+        if not os.path.exists(destdir):
+            try:
+                os.makedirs(destdir)
+            except OSError as exc:
+                if exc.errno == errno.EEXIST and os.path.isdir(destdir):
+                    pass
+                else:
+                    raise
+        reportdir = '/var/www/html/stonix/results'
+        reportfiles = os.listdir(reportdir)
+
+    except Exception:
+        message = traceback.format_exc()
+        log_error(message)
+        sys.exit(1)
+
     for reportfile in reportfiles:
-        repfile = os.path.join(reportdir, reportfile)
+
         try:
+
+            repfile = os.path.join(reportdir, reportfile)
+
             parser.openreport(repfile)
             reportdata = parser.parsereport()
             metadata = reportdata[0]
             findings = reportdata[1]
-        except Exception, err:
-            detailedresults = traceback.format_exc()
-            print err
-            print detailedresults
-            shutil.move(repfile, destdir)
-            continue
-        uploadip = reportfile.split('-')
-        uploadip = uploadip[0]
-        metadata['UploadAddress'] = uploadip
-        metadata['xmlFileName'] = reportfile
-        # print metadata
-        try:
+
+            uploadip = reportfile.split('-')
+            uploadip = uploadip[0]
+            metadata['UploadAddress'] = uploadip
+            metadata['xmlFileName'] = reportfile
+
             stonixdb.loaddata(metadata, findings)
-        except Exception, err:
-            print err
-            print metadata
-            shutil.move(repfile, destdir)
+
+            if os.path.exists(repfile):
+                shutil.move(repfile, destdir)
+
+        except KeyError:
+            message = traceback.format_exc()
+            log_error(message)
+            if os.path.exists(repfile):
+                shutil.move(repfile, destdir)
             continue
-        if os.path.exists(repfile):
-            shutil.move(repfile, destdir)
-    stonixdb.close()
+        except Exception:
+            message = traceback.format_exc()
+            log_error(message)
+            sys.exit(1)
+
+    try:
+
+        stonixdb.close()
+
+    except Exception:
+        message = traceback.format_exc()
+        log_error(message)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
