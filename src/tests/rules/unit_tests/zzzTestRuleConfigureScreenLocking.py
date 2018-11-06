fde23d23b9c80558a281b3c22133654d7748a076
Derek Walker <dwalker@lanl.gov>
2018-11-06 14:09:34 -0700

    updated unit test to configure files incorrectly and for testing

--- src/tests/rules/unit_tests/zzzTestRuleConfigureScreenLocking.py
+++ src/tests/rules/unit_tests/zzzTestRuleConfigureScreenLocking.py
@@ -21,6 +21,7 @@
 # See the GNU General Public License for more details.                        #
 #                                                                             #
 ###############################################################################
+from CodeWarrior.Standard_Suite import line
 '''
 This is a Unit Test for Rule ConfigureAppleSoftwareUpdate
 
@@ -101,8 +102,275 @@ class zzzTestRuleConfigureScreenLocking(RuleTest):
                                "askForPasswordDelay"]
                     self.logdispatch.log(LogPriority.DEBUG, str(command))
                     success = self.ch.executeCommand(command)
+        else:
+            success1 = self.setkde()
+            success2 = self.setgnome()
+            if success1 and success2:
+                success = True
+            else:
+                success = False
         return success
 
+    def setkde(self):
+        '''Method to setup kde desktop to not be compliant
+        @author: dwalker
+        @return: bool
+        '''
+        self.kdeprops = {"ScreenSaver": {"Enabled": "true",
+                                             "Lock": "true",
+                                             "LockGrace": "60000",
+                                             "Timeout": "840"}}
+        self.kderuin = []
+        success = True
+        bindir = glob("/usr/bin/kde*")
+        kdefound = False
+        for kdefile in bindir:
+            if re.search("^/usr/bin/kde\d$", str(kdefile)):
+                kdefound = True
+        if kdefound and self.environ.geteuid() == 0:
+            contents = readFile("/etc/passwd", self.logger)
+            if not contents:
+                debug = "You have some serious issues, /etc/passwd is blank\n"
+                self.logger.log(LogPriority.INFO, debug)
+                self.rulesuccess = False
+                self.detailedresults += "No contents found in /etc/passwd!\n"
+                return False
+            for line in contents:
+                temp = line.split(":")
+                try:
+                    if int(temp[2]) >= 500:
+                        if temp[5] and re.search('/', temp[5]):
+                            homebase = temp[5]
+                            if not re.search("^/home/", homebase):
+                                continue
+                            kfile = homebase + "/.kde/share/config/kscreensaverrc"
+                            if os.path.exists(kfile):
+                                uid = getpwnam(temp[0])[2]
+                                gid = getpwnam(temp[0])[3]
+                                if checkPerms(kfile, [uid, gid, 0o600],
+                                                  self.logger):
+                                    if not setPerms(kfile, [0, 0, 0o644],
+                                                    self.logger):
+                                        success = False
+                                        self.detailedresults += \
+                                        "Unable to set incorrect perms " + \
+                                        "on " + kfile + " for testing\n"
+                                if not self.wreckFile(kfile):
+                                    self.detailedresults += \
+                                        "Was not able to mess " + \
+                                        "up file for testing\n"
+                                    success = False
+                        else:
+                            debug += "placeholder 6 in /etc/passwd is not a \
+directory, invalid form of /etc/passwd"
+                            self.logger.log(LogPriority.DEBUG, debug)
+                            self.detailedresults += "Unexpected formatting in " + \
+                                "/etc/passwd"
+                            return False
+                except IndexError:
+                    success = False
+                    debug += traceback.format_exc() + "\n"
+                    debug += "Index out of range\n"
+                    self.logger.log(LogPriority.DEBUG, debug)
+                    self.detailedresults += "Unexpected formatting in " + \
+                        "/etc/passwd"
+                    break
+                except Exception:
+                    break
+        elif kdefound:
+            who = "/usr/bin/whoami"
+            message = Popen(who, stdout=PIPE, shell=False)
+            info = message.stdout.read().strip()
+            contents = readFile('/etc/passwd', self.logger)
+            if not contents:
+                debug += "You have some serious issues, /etc/passwd is blank\n"
+                self.logger.log(LogPriority.INFO, debug)
+                self.rulesuccess = False
+                self.detailedresults += "No contents found in /etc/passwd!\n"
+                return False
+            compliant = True
+            for line in contents:
+                temp = line.split(':')
+                try:
+                    if temp[0] == info:
+                        if temp[5] and re.search('/', temp[5]):
+                            homebase = temp[5]
+                            if not re.search("^/home/", homebase):
+                                continue
+                            kfile = homebase + "/.kde/share/config/kscreensaverrc"
+                            if os.path.exists(kfile):
+                                uid = getpwnam(temp[0])[2]
+                                gid = getpwnam(temp[0])[3]
+                                if checkPerms(kfile, [uid, gid, 0o600],
+                                                  self.logger):
+                                    if not setPerms(kfile, [0, 0, 0o644],
+                                                    self.logger):
+                                        success = False
+                                        self.detailedresults += \
+                                        "Unable to set incorrect perms " + \
+                                        "on " + kfile + " for testing\n"
+                                if not self.wreckFile(kfile):
+                                    self.detailedresults += \
+                                        "Was not able to mess " + \
+                                        "up file for testing\n"
+                                    success = False
+                        else:
+                            debug += "placeholder 6 in /etc/passwd is not a \
+directory, invalid form of /etc/passwd"
+                            self.logger.log(LogPriority.DEBUG, debug)
+                            self.detailedresults += "Unexpected formatting in " + \
+                                "/etc/passwd"
+                            return False
+                        break
+                except IndexError:
+                    success = False
+                    debug += traceback.format_exc() + "\n"
+                    debug += "Index out of range\n"
+                    self.logger.log(LogPriority.DEBUG, debug)
+                    self.detailedresults += "Unexpected formatting in " + \
+                        "/etc/passwd"
+                    break
+                except Exception:
+                    debug += traceback.format_exc() + "\n"
+                    self.logger.log(LogPriority.DEBUG, debug)
+                    break
+        return success
+
+    def setgnome(self):
+        '''Method to setup gnome desktop to not be compliant
+        @author: dwalker
+        @return: bool
+        '''
+        success = True
+        gconf = "/usr/bin/gconftool-2"
+        gsettings = "/usr/bin/gsettings"
+        dconfsettingslock = "/etc/dconf/db/local.d/locks/stonix-settings.conf"
+        dconflockdata = ["/org/gnome/desktop/session/idle-delay",
+                           "/org/gnome/desktop/session/idle-activation-enabled",
+                           "/org/gnome/desktop/screensaver/lock-enabled",
+                           "/org/gnome/desktop/screensaver/lock-delay",
+                           "/org/gnome/desktop/screensaver/picture-uri"]
+        dconfsettings = "/etc/dconf/db/local.d/local.key"
+        dconfdata = {"org/gnome/desktop/screensaver": {
+                                                    "idle-activation-enabled": "true",
+                                                    "lock-enabled": "true",
+                                                    "lock-delay": "0",
+                                                    "picture-opacity": "100",
+                                                    "picture-uri": "\'\'"},
+                                      "org/gnome/desktop/session": {
+                                                    "idle-delay": "uint32 900"}}
+        self.dconfuserprofile = "/etc/dconf/profile/user"
+        self.userprofilecontent = "user-db:user\n" + \
+                                          "system-db:local"
+        if os.path.exists(gconf):
+            setcmds1 = ["/apps/gnome-screensaver/idle_activation_enabled false",
+                       "/apps/gnome-screensaver/lock_enabled false"]
+            setcmds2 = "/desktop/gnome/session/idle_delay 5"
+            for cmd in setcmds:
+                cmd2 = gconf + " --type bool --set " + cmd
+            cmd2 = gconf + " --type int --set " + setcmds2
+        if os.path.exists(gsettings):
+            setcmds = [" set org.gnome.desktop.screensaver " +
+                       "idle-activation-enabled false",
+                       " set org.gnome.desktop.screensaver lock-enabled false",
+                       " set org.gnome.desktop.screensaver lock-delay 10",
+                       " set org.gnome.desktop.screensaver picture-opacity 50",
+                       " set org.gnome.desktop.session idle-delay 20"]
+            for cmd in setcmds:
+                cmd2 = gsettings + cmd
+                self.cmdhelper.executeCommand(cmd2)
+                if self.cmdhelper.getReturnCode() != 0:
+                    success = False
+                    info += "Unable to set value for " + cmd + \
+                        "using gsettings\n"
+        if self.environ.geteuid() == 0:
+            #write correct contents to dconf lock file
+            if os.path.exists(dconfsettingslock):
+                tempstring = ""
+                tmpfile = dconfsettingslock + ".tmp"
+                contents = readFile(dconfsettingslock, self.logger)
+                for line in contents:
+                    if line.strip() in dconflockdata:
+                        continue
+                    else:
+                        tempstring += line
+                if not writeFile(tmpfile, tempstring, self.logger):
+                    success = False
+                    self.detailedresults += "Unable to write contents to " + \
+                        "stonix-settings file\n"
+                else:
+                    os.rename(tmpfile, dconfsettingslock)
+                    os.chown(dconfsettingslock, 0, 0)
+                    os.chmod(dconfsettingslock, 493)
+                    resetsecon(dconfsettingslock)
+            #write correct contents to dconf lock file
+            if os.path.exists(self.dconfsettings):
+                self.kveditor = KVEditorStonix(self.statechglogger,
+                                               self.logger,
+                                               "tagconf",
+                                               dconfsettings,
+                                               dconfsettings + ".tmp",
+                                               dconfdata, "notpresent",
+                                               "closedeq")
+                if not self.kveditor.report():
+                    success = False
+                    self.detailedresults += "Unable to set incorrect contents " + \
+                        "for " + dconfsettings + "\n"
+                elif not self.kveditor.fix():
+                    success = False
+                    self.detailedresults += "Unable to set incorrect contents " + \
+                        "for " + dconfsettings + "\n"
+                elif not self.kveditor.commit():
+                    success = False
+                    self.detailedresults += "Unable to set incorrect contents " + \
+                        "for " + dconfsettings + "\n"
+            
+            if os.path.exists(dconfuserprofile):
+                fixing = False
+                contents = readFile(dconfuserprofile, self.logger)
+                contentstring = ""
+                for line in contents:
+                    contentstring += line
+                    if re.search(userprofilecontent, contentstring):
+                       fixing = True
+                if fixing:
+                    contentstring = ""
+                    tempfile = dconfuserprofile + ".tmp"
+                    if not writeFile(tempfile, contentstring, self.logger):
+                        success = False
+                        self.detailedresults += "Unable to set incorrect contents " + \
+                        "for " + dconfuserprofile + "\n"
+                    else:
+                        os.rename(tmpfile, dconfuserprofile)
+                        os.chown(dconfuserprofile, 0, 0)
+                        os.chmod(dconfuserprofile, 493)
+                        resetsecon(dconfuserprofile)
+
+    def wreckFile(self, filehandle):
+        '''Method to ensure correct contents are NOT in file for testing
+        @author: dwalker
+        @return: bool
+        @param filehandle: string
+        '''
+        self.editor = ""
+        kvt = "tagconf"
+        intent = "notpresent"
+        tpath = filehandle + ".tmp"
+        conftype = "closedeq"
+        self.editor = KVEditorStonix(self.statechglogger, self.logger, kvt,
+                                     filehandle, tpath, self.kdeprops, intent,
+                                     conftype)
+        if not self.editor.report():
+            if self.editor.fix():
+                if self.editor.commit():
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return True
+    
     def checkReportForRule(self, pCompliance, pRuleSuccess):
         '''
         check on whether report was correct
