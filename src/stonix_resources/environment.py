5b342b70e653c9cb474169522313c1a7009d5580
Breen Malmberg <bemalmbe@lanl.gov>
2018-12-05 09:35:03 -0700

    [noartf] troubleshooting network issues from qa test request on debian/ubuntu

--- src/stonix_resources/environment.py
+++ src/stonix_resources/environment.py
@@ -541,7 +541,11 @@ class Environment:
             print "environment::getmacaddr():WARNING: Could not detect any net utility type/location"
             return macaddr
         elif "nmcli" in netutil:
-            netcmd = netutil +  " -t dev list"
+            # fork fork fork...
+            if re.search("debian|ubuntu", self.getostype(), re.IGNORECASE):
+                netcmd = netutil + " device show"
+            else:
+                netcmd = netutil +  " -t dev list"
         elif "ifconfig" in netutil:
             netcmd = netutil + " -a"
         elif "ip" in netutil:
