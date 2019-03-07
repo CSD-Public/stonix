853151639f80effa4aa11efcaf1a902ab65241a1
Brandon R. Gonzales <bgonz12@lanl.gov>
2019-03-07 10:05:52 -0700

    [noartf] NoCoreDumps: Fix mac traceback

--- src/stonix_resources/rules/NoCoreDumps.py
+++ src/stonix_resources/rules/NoCoreDumps.py
@@ -411,7 +411,7 @@ class NoCoreDumps(Rule):
         success = True
 
         try:
-
+            self.created1 = False
             self.cmdhelper = CommandHelper(self.logger)
 
             if self.environ.getostype() == "Mac OS X":
