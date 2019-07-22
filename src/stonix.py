630b363e155c24e49071523b551f9aec66485ed6
Derek Walker <dwalker@lanl.gov>
2019-07-22 13:24:01 -0600

    latest

--- src/stonix.py
+++ src/stonix.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 ###############################################################################
 #                                                                             #
 # Copyright 2019. Triad National Security, LLC. All rights reserved.          #
