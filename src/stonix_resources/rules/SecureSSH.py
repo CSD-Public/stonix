7de48bababc3eeee15718206fc8c24d58e84648e
Breen Malmberg <bemalmbe@lanl.gov>
2019-10-29 08:59:02 -0600

    [noartf] removing the cbc ciphers from securessh config in 0.9.32

--- src/stonix_resources/rules/SecureSSH.py
+++ src/stonix_resources/rules/SecureSSH.py
@@ -136,7 +136,7 @@ class SecureSSH(Rule):
                            "GSSAPIAuthentication": "yes",
                            "GSSAPICleanupCredentials": "yes",
                            "UsePAM": "yes",
-                           "Ciphers": "aes128-ctr,aes192-ctr,aes256-ctr,aes128-cbc,3des-cbc,aes192-cbc,aes256-cbc",
+                           "Ciphers": "aes128-ctr,aes192-ctr,aes256-ctr",
                            "PermitUserEnvironment": "no"}
 
             if self.environ.getostype() == "Mac OS X":
